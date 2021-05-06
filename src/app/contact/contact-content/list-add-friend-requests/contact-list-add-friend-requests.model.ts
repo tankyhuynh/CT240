dcdc513c424f7b4409a8c35ba3977f760d6c3c28
chuongquan12 <chuongquan12@gmail.com>
2021-05-06 20:26:41 +0700

    Modify UI personal-information

--- src/app/contact/contact-content/list-add-friend-requests/contact-list-add-friend-requests.model.ts
+++ src/app/contact/contact-content/list-add-friend-requests/contact-list-add-friend-requests.model.ts
@@ -1,14 +1,15 @@
 export interface ListAddFriendRequestModel {
-  _id: string,
+  _id: string;
   sender: {
-      _id: string,
-      name: string,
-      avatar: string
-    },
+    _id: string;
+    name: string;
+    avatar: string;
+  };
+  introduce: string;
   receiver: {
-      _id: string,
-      name: string,
-      avatar: string
-    },
-  sended_at: Date
+    _id: string;
+    name: string;
+    avatar: string;
+  };
+  sended_at: Date;
 }
