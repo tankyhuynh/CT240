1ce641a1f2a95cb11a26fa2924d2daf2bc190d1b
chuongquan12 <chuongquan12@gmail.com>
2021-05-09 16:52:14 +0700

    Modify Scroll

--- src/app/chat/chat-roomchat/chat-roomchat-body/chat-roomchat-body.component.ts
+++ src/app/chat/chat-roomchat/chat-roomchat-body/chat-roomchat-body.component.ts
@@ -1,54 +1,59 @@
-import { AfterViewChecked, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewChecked,
+  Component,
+  ElementRef,
+  Input,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { RoomModel } from 'src/app/contact/contact-content/contact-add-room/contact-add-room.model';
 import { ContactListRoomService } from 'src/app/contact/contact-content/list-rooms/contact-list-rooms.service';
 import { SharingService } from 'src/app/sharing.service';
 import { SocketService } from 'src/app/socket/socket.service';
 import { MessageModel } from '../chat-roomchat-message.model';
 
-import { ChatRoomModel } from '../chatroom.model'
+import { ChatRoomModel } from '../chatroom.model';
 
 @Component({
   selector: 'app-chat-roomchat-body',
   templateUrl: './chat-roomchat-body.component.html',
-  styleUrls: ['./chat-roomchat-body.component.css']
+  styleUrls: ['./chat-roomchat-body.component.css'],
 })
 export class ChatRoomchatBodyComponent implements OnInit, AfterViewChecked {
-
   @Input() chatroom: RoomModel;
   @Input() messages: MessageModel[] = [];
   currentUserId: string = localStorage.getItem('userId');
 
-  @ViewChild("scrollMe") myScroller: ElementRef<any>;
+  top: number;
+
+  @ViewChild('scrollMe') myScroller: ElementRef<any>;
 
   constructor(
     private roomService: ContactListRoomService,
     private sharingService: SharingService,
     private socketService: SocketService
-  ) { }
+  ) {}
 
   ngOnInit(): void {
-    this.socketService
-      .getMessages()
-      .subscribe((message: MessageModel) => {
-        this.messages.push(message);
-        console.log('ngOnInit() chat room');
-        console.log(this.messages);
-      });
+    this.top = 1;
+    this.socketService.getMessages().subscribe((message: MessageModel) => {
+      this.messages.push(message);
+      console.log('ngOnInit() chat room');
+      console.log(this.messages);
+    });
   }
 
-  ngAfterViewChecked(){
+  ngAfterViewChecked() {
     this.scrollToBottom();
   }
 
-
-  scrollToBottom():void {
+  scrollToBottom(): void {
     try {
-      this.myScroller.nativeElement.scrollTop = this.myScroller.nativeElement.scrollHeight;
-    } catch (error) {
-
-    }
+      var height = this.myScroller.nativeElement.scrollHeight;
+      if (this.top < height) {
+        this.myScroller.nativeElement.scrollTop = this.myScroller.nativeElement.scrollHeight;
+        this.top = this.myScroller.nativeElement.scrollHeight;
+      }
+    } catch (error) {}
   }
-
-
-
 }
