bd2a3ea45376f1b4b3912d705a169b3d48c64293
chuongquan12 <chuongquan12@gmail.com>
2021-05-12 22:31:41 +0700

    Modify UI + icon list rooms + settimeout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,22 +1,21 @@
-import { HttpClient } from "@angular/common/http";
-import { elementEventFullName } from "@angular/compiler/src/view_compiler/view_compiler";
-import { Injectable } from "@angular/core";
-import { Router } from "@angular/router";
-import { BehaviorSubject } from "rxjs";
-import { Subject } from "rxjs";
+import { HttpClient } from '@angular/common/http';
+import { elementEventFullName } from '@angular/compiler/src/view_compiler/view_compiler';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { Subject } from 'rxjs';
 
 import { environment } from '../../environments/environment';
-import { ProfileModel } from "../personal-information/profile.model";
-import { ProfileService } from "../personal-information/profile.service";
-import { SharingService } from "../sharing.service";
-import { AuthData } from "./auth.model";
-import { UserData } from "./user.model";
+import { ProfileModel } from '../personal-information/profile.model';
+import { ProfileService } from '../personal-information/profile.service';
+import { SharingService } from '../sharing.service';
+import { AuthData } from './auth.model';
+import { UserData } from './user.model';
 
-const BACKEND_URL = environment.apiUrl + "/users/";
+const BACKEND_URL = environment.apiUrl + '/users/';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class AutheService {
-
   private token: string;
   private tokenTimer: any;
   private authStatusListener = new Subject<boolean>();
@@ -25,25 +24,23 @@ export class AutheService {
   private userId: string;
   private userData: UserData;
 
-
-  getUserId(){
+  getUserId() {
     return this.userId;
   }
 
-  getToken(){
+  getToken() {
     return this.token;
   }
 
-  getReloginStatusListener(){
+  getReloginStatusListener() {
     return this.reloginStatusListener.asObservable();
   }
 
-  getAuthStatusListener(){
+  getAuthStatusListener() {
     return this.authStatusListener.asObservable();
   }
 
-
-  getIsAuthenticated(){
+  getIsAuthenticated() {
     return this.isAuthenticated;
   }
 
@@ -51,68 +48,90 @@ export class AutheService {
     private http: HttpClient,
     private router: Router,
     private profileService: ProfileService,
-    private sharingService: SharingService){};
-
-
+    private sharingService: SharingService
+  ) {}
 
   createUser(name: string, phone: string, password: string, avatar: string) {
-    const authData: UserData = {name: name, phone: phone, password: password, avatar: avatar };
-    this.http
-      .post(BACKEND_URL + "register", authData)
-      .subscribe(() => {
+    const authData: UserData = {
+      name: name,
+      phone: phone,
+      password: password,
+      avatar: avatar,
+    };
+    this.http.post(BACKEND_URL + 'register', authData).subscribe(
+      () => {
         this.router.navigate(['/auth/login']);
-      }, error => {
+      },
+      (error) => {
         this.authStatusListener.next(false);
         this.router.navigate(['/auth/signup']);
-      });
+      }
+    );
   }
 
-  confirmLogin(phone: string, password: string){
-    const authData: AuthData = {phone: phone, password: password};
+  confirmLogin(phone: string, password: string) {
+    const authData: AuthData = { phone: phone, password: password };
     this.http
-          .post<{data: {_id: string, token: string, expiresIn: number}}>(BACKEND_URL + "login", authData)
-            .subscribe(response => {
-              this.token = response.data.token;
-              if ( this.token ) {
-                this.sharingService.changeReloginStatus(true);
-                // this.router.navigate(['/']);
-              }
-            }, err => {
-              this.sharingService.changeReloginStatus(false);
-            });
+      .post<{ data: { _id: string; token: string; expiresIn: number } }>(
+        BACKEND_URL + 'login',
+        authData
+      )
+      .subscribe(
+        (response) => {
+          this.token = response.data.token;
+          if (this.token) {
+            this.sharingService.changeReloginStatus(true);
+            // this.router.navigate(['/']);
+          }
+        },
+        (err) => {
+          this.sharingService.changeReloginStatus(false);
+        }
+      );
   }
 
-  login(mobilePhone: string, password: string){
-
-    const authData: AuthData = {phone: mobilePhone, password: password};
+  login(mobilePhone: string, password: string) {
+    const authData: AuthData = { phone: mobilePhone, password: password };
     this.http
-          .post<{data: {_id: string, token: string, expiresIn: number}}>(BACKEND_URL + "login", authData)
-            .subscribe(response => {
-              this.token = response.data.token;
-              if ( this.token ) {
-                const expiresInDuration =  response.data.expiresIn;
-                this.setAuthTimer(expiresInDuration);
-                this.isAuthenticated = true;
-                this.userId = response.data._id;
+      .post<{ data: { _id: string; token: string; expiresIn: number } }>(
+        BACKEND_URL + 'login',
+        authData
+      )
+      .subscribe(
+        (response) => {
+          this.token = response.data.token;
+          if (this.token) {
+            const expiresInDuration = response.data.expiresIn;
+            this.setAuthTimer(expiresInDuration);
+            this.isAuthenticated = true;
+            this.userId = response.data._id;
 
-                this.authStatusListener.next(true);
-                const now = new Date();
-                const expirationDate = new Date(now.getTime() + (expiresInDuration * 1000));
+            this.authStatusListener.next(true);
+            const now = new Date();
+            const expirationDate = new Date(
+              now.getTime() + expiresInDuration * 1000
+            );
 
-                this.profileService.getOneById(this.userId);
+            this.profileService.getOneById(this.userId);
 
-                this.saveDataToLocalStorage(this.token, expirationDate, this.userId, this.userData );
+            this.saveDataToLocalStorage(
+              this.token,
+              expirationDate,
+              this.userId,
+              this.userData
+            );
 
-                this.router.navigate(['/chat']);
-
-              }
-            }, err => {
-              this.router.navigate(['/auth/login']);
-            });
-            return false;
+            this.router.navigate(['/chat']);
+          }
+        },
+        (err) => {
+          this.router.navigate(['/auth/login']);
+        }
+      );
+    return false;
   }
 
-  logout(){
+  logout() {
     this.token = null;
     this.isAuthenticated = false;
     this.userId = null;
@@ -122,25 +141,30 @@ export class AutheService {
     this.router.navigate(['/auth/login']);
   }
 
-  private saveDataToLocalStorage(token: string, expiration: Date, userId: string, userData: ProfileModel){
+  private saveDataToLocalStorage(
+    token: string,
+    expiration: Date,
+    userId: string,
+    userData: ProfileModel
+  ) {
     localStorage.setItem('token', token);
     localStorage.setItem('expiration', expiration.toISOString());
     localStorage.setItem('userId', userId);
     localStorage.setItem('userData', JSON.stringify(userData));
   }
 
-  private clearLocalStorage(){
+  private clearLocalStorage() {
     localStorage.clear();
   }
 
-  private setAuthTimer(duration: number){
-    this.tokenTimer = setTimeout( () => {
+  private setAuthTimer(duration: number) {
+    this.tokenTimer = setTimeout(() => {
       this.logout();
-    }, duration * 1000);
-    console.log("Setting timer: " + duration);
+    }, duration * 1000 * 60);
+    console.log('Setting timer: ' + duration);
   }
 
-  autoAuthUser(){
+  autoAuthUser() {
     const authInformation = this.getLocalStorageData();
     if (!authInformation) {
       return;
@@ -148,33 +172,29 @@ export class AutheService {
 
     const now = new Date();
     const expiredIn = authInformation.expirationDate.getTime() - now.getTime();
-    if ( expiredIn > 0 ) {
+    if (expiredIn > 0) {
       this.token = authInformation.token;
       this.userId = authInformation.userId;
 
       this.isAuthenticated = true;
-      this.setAuthTimer( expiredIn/1000 );
+      this.setAuthTimer(expiredIn / 1000);
       this.authStatusListener.next(true);
-
     }
   }
 
-  private getLocalStorageData(){
+  private getLocalStorageData() {
     const token = localStorage.getItem('token');
     const expirationDate = localStorage.getItem('expiration');
     const userId = localStorage.getItem('userId');
     let userData;
 
-    if ( !token || !expirationDate ) {
+    if (!token || !expirationDate) {
       return;
     }
     return {
       token: token,
       expirationDate: new Date(expirationDate),
-      userId: userId
-    }
+      userId: userId,
+    };
   }
-
-
-
 }
