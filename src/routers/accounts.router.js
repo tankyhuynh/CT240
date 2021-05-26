70b63096a9cb652bd889c319ce7a34e6dcaac831
Dev-ThanhNhan <it.thanhnhan.mientay@gmail.com>
2021-05-26 20:51:02 +0700

    delete all

--- src/routers/accounts.router.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const AccountController = require('../controllers/account.controller');
-
-router.put('/', AccountController.update);
-module.exports = router;
--- src/routers/users.router.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const UsersController = require('../controllers/user.controller');
-
-router.post("/login", UsersController.login);
-router.post("/register", UsersController.register);
-router.delete("/logout", UsersController.logout);
-
-module.exports = router;
